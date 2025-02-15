343ff466aab24741b9d59db328f8f3ee7bd88b40
Damien <angibaud.pro@gmail.com>
2025-02-15 19:25:09 +0100

    init routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header style={{ backgroundColor: "#282c34", padding: "10px", color: "white", textAlign: "center" }}>
       <h1>Gestion des utilisateurs</h1>
       <nav>
-        <Link to="/learnReact/home">Accueil</Link> | <Link to="/learnReact/add">Ajouter</Link>
+        <Link to="/">Accueil</Link> | <Link to="/add">Ajouter</Link>
       </nav>
     </header>
   );
@@ -85,8 +85,8 @@ const App = () => {
     <Router>
       <Header />
       <Routes>
-        <Route path="/learnReact/home" element={<Home />} />
-        <Route path="/learnReact/add" element={<AddUser />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddUser />} />
       </Routes>
     </Router>
   );
